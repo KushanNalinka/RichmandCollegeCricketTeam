9c9d7bd9510acb996ee7861ece0319e0f9759acc
WLNUmaya <mamthaoshadi@gmail.com>
2024-09-12 12:01:59 +0530

    Adding Images and edited tailwind cofiguration, App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Initial from './pages/Initial.js';
@@ -8,24 +7,23 @@ import PlayerInfo from './pages/PlayerInfo.js';
 import MatchDetail from './pages/MatchDetail.js';
 import ScoreCardPage from './components/ScoreCardPage.js';
 import Team from './pages/Team.js';
-
-// Create this component if it doesn't exist
+import MatchInfo from './pages/MatchInfo.js';
+import ScorecardData from './pages/ScoreCardData.js';
 
 function App() {
   return (
     <BrowserRouter>
-        <Routes>
+      <Routes>
         <Route path="/" element={<Initial />} />
-         <Route path="/profile" element={<Profile />} />
-         <Route path="/coach" element={<Coaches />} />
-         <Route path='/player' element={<PlayerInfo/>} />
-         <Route path='/match' element={<MatchDetail/>} />
-         <Route path="/scorecard/:matchId" element={<ScoreCardPage />} />
-         <Route path="/team" element={<Team />} />
-
-        </Routes>
-      
-   
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/coach" element={<Coaches />} />
+        <Route path="/player" element={<PlayerInfo />} />
+        <Route path="/match" element={<MatchDetail />} />
+        <Route path="/scorecard/:matchId" element={<ScoreCardPage />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/match-info" element={<MatchInfo />} />
+        <Route path="/scorecard" element={<ScorecardData />} />
+      </Routes>
     </BrowserRouter>
   );
 }

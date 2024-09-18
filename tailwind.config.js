c60e39aa39e28fd91ad1afa607c907d8cbd6171f
WijesekaraDNM <masha.wijesekara@gmail.com>
2024-09-18 11:24:10 +0530

    Commiting player profile

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,21 +10,22 @@ export default {
     extend: {
 
       colors: {
-        baseBlue:'#00175F',
-        baseBlue2:'#002488',
-        baseRed1:'#802F3B',
-        baseRed2:'#823944'
+        // baseBlue:'#00175F',
+        // baseBlue2:'#002488',
+        // baseRed1:'#802F3B',
+        // baseRed2:'#823944'
+        baseBlue:'#08165A',
+        baseBlue2:'#112480',
+        baseRed1:'#480D35',
+        baseRed2:'#5D1245'
       },
       backgroundImage: {
         'Background': "url('./src/assets/images/wallback.png')",
       },      
-    }
-
       fontSize: {
         'xxs': '0.625rem', // Custom font size for screens smaller than xs
       },
     },
-
   },
   plugins: [],
 }

6ed51299a3b21aee57ac4c99cd5c867777a35602
Kushan Nalinka <kushannalinka572@gmail.com>
2024-09-24 12:08:05 +0530

    Frontend Changes Implementation
    Frontend Changes Implementation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 import Initial from './pages/Initial.js';
 import Profile from './pages/Profile.js';

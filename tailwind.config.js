f2f0f292d455709832a9c38f44dd78d146a57d7e
WLNUmaya <mamthaoshadi@gmail.com>
2024-09-27 15:14:48 +0530

    Score Card Data, Match Info Updations
    Score Card Data Manipulations

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,7 +27,9 @@ module.exports = {
         'sans': ['Inter', 'sans-serif'],
       },
       fontSize: {
-        'xxs': '0.625rem', // Custom font size for screens smaller than xs
+        'xxs': '0.625rem',  // Extra small size
+        'xxxs': '0.5rem',   // Extra extra small size
+        'xxxxs': '0.375rem' // Extra extra extra small size
       },
     },
   },

a7ef233df25327940e42f11553f8f90d6f38a5bb
Nuwani Wijesekara <masha.wijesekara@gmail.com>
2024-10-24 13:19:10 +0530

    Changed <AuthContexProvider> into <AuthProvider>

--- src/index.js
+++ src/index.js
@@ -6,13 +6,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AuthContexProvider } from './context/authContext';
+import { AuthProvider } from './hooks/UseAuth';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AuthContexProvider>
-    <App />
-    </AuthContexProvider>
+    {/* <AuthContexProvider> */}
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+    
+    {/* </AuthContexProvider> */}
   </React.StrictMode>
 );
 

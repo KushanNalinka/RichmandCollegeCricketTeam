7e5904253584f733bf13c0c72a698cbe7262f2bf
Nuwani Wijesekara <masha.wijesekara@gmail.com>
2024-10-25 06:06:12 +0530

    ROUTE CHANGES

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,45 @@
+// // src/components/PrivateRoute.js
+// import React from 'react';
+// import { Navigate } from 'react-router-dom';
+// import { useAuth } from '../hooks/UseAuth';
+
+// const PrivateRoute = ({ children, allowedRoles }) => {
+//   const { isAuthenticated, userRole } = useAuth();
+
+//   if (!isAuthenticated) {
+//     return <Navigate to="/" />;
+//   }
+
+//   // Redirect if the user doesn't have an allowed role
+//   if (allowedRoles && !allowedRoles.includes(userRole)) {
+//     return <Navigate to="/" />;
+//   }
+
+//   return children;
+// };
+
+// export default PrivateRoute;
+
+// src/components/PrivateRoute.js
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../hooks/UseAuth';
+
+const PrivateRoute = ({ children, allowedRoles }) => {
+  const { isAuthenticated, userRole } = useAuth();
+  console.log("userRole: ",userRole);
+    console.log("allowableUser: ",allowedRoles);
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+
+  if (allowedRoles && !allowedRoles.includes(userRole)) {
+    
+    return <Navigate to="/member" />; // Redirect to home if role is not allowed
+  }
+
+  return children;
+};
+
+export default PrivateRoute;
+

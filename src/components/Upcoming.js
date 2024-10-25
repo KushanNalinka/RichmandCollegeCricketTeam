b91c70fce98134103a464cda44a67ddd122aa76d
WLNUmaya <mamthaoshadi@gmail.com>
2024-10-25 14:25:43 +0530

    ipcoming layout color changes

--- src/components/Upcoming.js
+++ src/components/Upcoming.js
@@ -56,14 +56,14 @@ export default function Upcoming({ selectedAgeGroup, selectedMatchType }) {
         matchDataList.map((matchData, index) => (
           <div
             key={index}
-            className="bg-[#012D5E]/70 text-white rounded-3xl shadow-lg p-4 w-full max-w-5xl h-auto flex flex-row items-center justify-between space-x-4"
+            className="bg-white text-[#012D5E] rounded-3xl shadow-lg p-4 w-full max-w-5xl h-auto flex flex-row items-center justify-between space-x-4 font-bold"
           >
             {/* Richmond College Info */}
-            <div className="flex flex-col items-center space-y-2 w-1/4">
+            <div className="flex flex-col items-center space-y-2 w-1/4 font-bold">
             <img
                 src={require('../assets/images/LOGO.png')} // Dynamic import for assets in React
                 alt="RICHMOND COLLEGE"
-                className="w-10 h-10 sm:w-10 sm:h-10 rounded-full"
+                className="w-10 h-10 sm:w-10 sm:h-10 "
               />
               <div className="text-center">
                 <h3 className="text-xxs font-semibold tracking-wide sm:text-xs">
@@ -75,7 +75,7 @@ export default function Upcoming({ selectedAgeGroup, selectedMatchType }) {
             {/* VS Section */}
             <div className="flex flex-col items-center justify-center">
               <div className="h-6 w-px bg-gradient-to-b from-transparent via-white to-transparent sm:h-12" />
-              <span className="text-white text-sm sm:text-sm my-2">VS</span>
+              <span className="text-[#012D5E] text-sm sm:text-sm my-2">VS</span>
               <div className="h-6 w-px bg-gradient-to-t from-transparent via-white to-transparent sm:h-12" />
             </div>
             {/* Opposition Info */}
@@ -94,11 +94,11 @@ export default function Upcoming({ selectedAgeGroup, selectedMatchType }) {
               </div>
             </div>
             {/* Match Info */}
-            <div className="w-1/2 p-2 text-left flex flex-col items-start">
+            <div className="w-1/2 p-2 text-left flex flex-col items-start text-[#012D5E] font-bold">
               <h4 className="text-xxs font-bold text-[#53A2F6] sm:text-xs">
                 RICHMOND VS {matchData.opposition ? matchData.opposition.toUpperCase() : "UNKNOWN OPPONENT"} {matchData.type ? matchData.type.toUpperCase() : ""}
               </h4>
-              <p className="text-xxs sm:text-xs mt-2 text-[#B4CDDC]">
+              <p className="text-xxs sm:text-xs mt-2 text-black">
                 {matchData.venue ? matchData.venue : "Unknown Venue"} - {matchData.date ? formatDate(matchData.date) : "Unknown Date"} at {matchData.time ? matchData.time : "Unknown Time"}
               </p>
               <p className="text-xxs sm:text-xs mt-2 font-semibold">

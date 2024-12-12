da29b80e94df6704963167b5dff42f0a8e1075bd
Kushan Nalinka <kushannalinka572@gmail.com>
2024-12-13 00:01:03 +0530

    App.js Modifications
    App.js Modifications

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
      <AuthContextProvider>
-     
+     <AuthProvider>
         <App />
-       
+        </AuthProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
